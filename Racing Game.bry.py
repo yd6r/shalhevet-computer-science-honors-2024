b9e9fb9603402fc7dab78a7853bf20931e2e83d6
yd6r <2027.y.deitcher@shalhevet.org>
2024-11-06 11:47:36 -0800

    Update Racing Game.bry.py
    This new version reads both the circle and square from the right end(whereas previously the square was read from the top left corner and the circle was read from the center) and places them next to each other and at the far left of the screen.

--- Racing Game.bry.py
+++ Racing Game.bry.py
@@ -1,27 +1,27 @@
 cir_win_txt=Text("Green Wins!")
 sq_win_txt=Text("Red Wins!")
 def winner():
-    if cir_x() >= 380:
+    if cir_x() >= 380-24:
         cir_win_txt.set_position(70,220)
         cir_win_txt.set_color(Color.green)
         cir_win_txt.set_font("40pt Helvetica")
         add(cir_win_txt)
-        remove(sq_win_txt)
-    elif sq_x() >= 380:
+        sq.set_position(-100,3)
+    elif sq_x() >= 380-49:
         sq_win_txt.set_color(Color.red)
         sq_win_txt.set_position(70,220)
         sq_win_txt.set_font("40pt Helvetica")
         add(sq_win_txt)
-        remove(cir_win_txt)
-  
+        circle.set_position(-100,3)
+        
 
 circle=Circle(25)
-circle.set_position(100,300)
+circle.set_position(27,300)
 circle.set_color(Color.green)
 add(circle)
 
 sq=Rectangle(50,50)
-sq.set_position(100,100)
+sq.set_position(1,100)
 sq.set_color(Color.red)
 add(sq)
 
@@ -47,7 +47,6 @@ def move_square(event):
         circle.move(0,15)
     winner()
 
-
 def goalpost():
     rect=Rectangle(15,50)
     rect.set_position(380,20)
@@ -64,9 +63,6 @@ def goalpost():
     
 add_key_up_handler(move_square)
 goalpost()
-
-
-        
 
 txt=Text("Race to the yellow line")
 txt.set_position(20,420)
