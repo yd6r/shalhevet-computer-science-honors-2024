2cdc0c3ca9e618e43746add2d53001431a95c984
yd6r <2027.y.deitcher@shalhevet.org>
2024-11-20 11:12:11 -0800

    Update Racing Game.bry.py
    This version changes the winning font and tries to make borders in the top and bottom of the screen which the two racers cannot cross, but the collision is not working, so they go right through.

--- Racing Game.bry.py
+++ Racing Game.bry.py
@@ -4,22 +4,31 @@ red_leading_txt=Text("RED IS WINNING")
 green_leading_txt=Text("GREEN IS WINNING")
 green_leading_txt.set_color(Color.green)
 red_leading_txt.set_color(Color.red)
+green_leading_txt.set_font("10pt Pacifico")
+red_leading_txt.set_font("10pt Pacifico")
+
+bottom_line=Line(1,450,379,450)
+add(bottom_line)
+top_line=Line(1,30,379,30)
+add(top_line)
 
 def winner():
     if cir_x() >= 380:
         remove(green_leading_txt)
         cir_win_txt.set_position(70,220)
         cir_win_txt.set_color(Color.green)
-        cir_win_txt.set_font("40pt Helvetica")
+        cir_win_txt.set_font("40pt Impact")
         add(cir_win_txt)
+        remove(red_leading_txt)
         sq.set_position(-100,3)
     elif sq_x() >= 380:
         remove(red_leading_txt)
         sq_win_txt.set_color(Color.red)
         sq_win_txt.set_position(70,220)
-        sq_win_txt.set_font("40pt Helvetica")
+        sq_win_txt.set_font("40pt Impact")
         add(sq_win_txt)
         circle.set_position(-100,3)
+        remove(green_leading_txt)
     if cir_x() > sq_x() and cir_x() < 380:
         green_leading_txt.set_position(100,50)
         remove(red_leading_txt)
@@ -31,7 +40,32 @@ def winner():
         remove(red_leading_txt)
         add(red_leading_txt)
         
-        
+top_y=top_line.get_y()
+bottom_y=bottom_line.get_y()
+
+
+def cir_x(): return circle.get_x()-24
+def sq_x(): return sq.get_x()
+def cir_y(): return circle.get_y()
+def sq_y(): return sq.get_y()
+
+
+
+def wall_collision():
+    print(top_y, bottom_y)
+   # print("working")  
+    if top_y>=cir_y:
+        #circle.set_position(cir_x, top_y)
+        print("out of bounds")
+    if top_y>=sq_y:
+        sq.set_position(sq_x,top_y)
+        print("out of bounds")
+    if bottom_y<=cir_y:
+        circle.set_position(cir_x,bottom_y)
+        print("out of bounds")
+    if bottom_y<=sq_y:
+        sq.set_position(sq_x,bottom_y)
+        print("out of bounds")
         
 circle=Circle(25)
 circle.set_position(27,300)
@@ -42,11 +76,6 @@ sq=Rectangle(50,50)
 sq.set_position(1,100)
 sq.set_color(Color.red)
 add(sq)
-
-def cir_x(): return circle.get_x()-24
-def sq_x(): return sq.get_x()
-def cir_y(): return circle.get_y()
-def sq_y(): return sq.get_y()
 
 def move_square(event):
     if event.key=="ArrowRight":
@@ -66,6 +95,7 @@ def move_square(event):
     if event.key=="s":
         circle.move(0,15)
     winner()
+    wall_collision()
 
 def goalpost():
     rect=Rectangle(15,50)
