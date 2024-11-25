fac488f624bfe3985775b15052e78a12189bb389
yd6r <2027.y.deitcher@shalhevet.org>
2024-11-25 12:11:56 -0800

    Create Pythagorean Triplets.py
    Prints the Pythagorean triplets which add to 1000, and them added and squared

---
+++ Pythagorean Triplets=1000.py
@@ -0,0 +1,10 @@
+a=1
+b=1
+c=1
+for a in range(1,1000):
+   for b in range(a,1000):
+        c = 1000-a-b
+        if (a**2 + b**2) == c**2 and a+b+c==1000:
+               print(a, b, c)
+               print(a+b+c)
+               print(a**2+b**2, c**2)
