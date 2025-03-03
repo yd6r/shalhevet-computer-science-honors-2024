9f393da70b28809653a86f9745e5d34e6ce43588
Yehudah Deitcher <2027.y.deitcher@shalhevet.org>
2025-03-03 12:09:39 -0800

    Update multiplicative cipher.py

--- multiplicative cryptography.py
+++ multiplicative cryptography.py
@@ -1,12 +1,13 @@
 import math
 
-  
-#The 'alphabet' is specific to this program, and does not contain many basic characters such as [], &, and "".
+# The 'alphabet' is specific to this program, and does not contain many basic characters such as [], &, and "".
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?',.() "
 
-#Checks if the key is coprime with the alphabet
+
+# Checks if the key is coprime with the alphabet
 def are_coprime(a, b):
     return math.gcd(a, b) == 1
+
 
 # Function to find the modular inverse of a number
 def modular_inverse(a, m):
@@ -26,10 +27,10 @@ def modular_inverse(a, m):
     return x1
 
 
-#Takes parameter 'multiply_key', the number which will be used in the multiplicative algorithm, and then asks for a message.
-#The key MUST be prime or the program will not work correctly.
-#The message is then run through the multiplicative algorithm to produce the encoded message. The encoded message will change
-#depending on the value of 'multiply_key'.
+# Takes parameter 'multiply_key', the number which will be used in the multiplicative algorithm, and then asks for a message.
+# The key MUST be prime or the program will not work correctly.
+# The message is then run through the multiplicative algorithm to produce the encoded message. The encoded message will change
+# depending on the value of 'multiply_key'.
 def multiplicative_encode(multiply_key):
     message = input("What message would you like to encode?")
     for letter in message:
@@ -58,25 +59,29 @@ def multiplicative_decode(multiply_key):
             convert = (pos * inverse_key) % len(alphabet)
             print(alphabet[convert], end="")
 
-#This is the part the user interacts with. It asks the user to choose to encode or decode a message, and if the response
-#matches neither, resets. It will run functions encode_multiplicative() or decode_multiplicative to encode or decode
-#a given message.
-encode_or_decode=input("For encoding a message, type 'encode'. For decoding a message, type 'decode'.")
+
+# This is the part the user interacts with. It asks the user to choose to encode or decode a message, and if the response
+# matches neither, resets. It will run functions encode_multiplicative() or decode_multiplicative to encode or decode
+# a given message.
+encode_or_decode = input("For encoding a message, type 'encode'. For decoding a message, type 'decode'.")
 if encode_or_decode != "encode" and encode_or_decode != "decode":
     while encode_or_decode != "encode" and encode_or_decode != "decode":
-        print("'"+encode_or_decode+"' is not a valid response.")
+        print("'" + encode_or_decode + "' is not a valid response.")
         encode_or_decode = input("For encoding a message, type 'encode'. For decoding a message, type 'decode'.")
 
-if encode_or_decode=="encode":
+if encode_or_decode == "encode":
     try:
         multiply_key = int(input("What is the key?"))
-    except ValueError: multiply_key=int(input("That is not a number key. Please enter a valid key."))
-    while not are_coprime(multiply_key, len_alphabet()):
-                        multiply_key=int(input("That key is not coprime with the alphabet. Please enter a valid key."))
+    except ValueError:
+            multiply_key = int(input("That is not a number key. Please enter a valid key."))
+    while not are_coprime(multiply_key, len(alphabet)):
+        multiply_key = int(input("That key is not coprime with the length of the alphabet used here("+str(len(alphabet))+"). Please enter a valid key."))
     multiplicative_encode(multiply_key)
-elif encode_or_decode=="decode":
+elif encode_or_decode == "decode":
     try:
         multiply_key = int(input("What is the key?"))
-    except ValueError: multiply_key= int(input("That is not a valid key. Please enter a valid key."))
+    except ValueError:
+        multiply_key = int(input("That is not a number key. Please enter a valid key."))
+    while not are_coprime(multiply_key, len(alphabet)):
+        multiply_key = int(input("That key is not coprime with the length of the alphabet used here("+str(len(alphabet))+"). Please enter a valid key."))
     multiplicative_decode(multiply_key)
-
