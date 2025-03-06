02772ecf2c74f1739c3dbf2e85ad6b6a47dd83f7
Yehudah Deitcher <2027.y.deitcher@shalhevet.org>
2025-03-06 14:37:59 -0800

    Update multiplicative cryptography.py

--- multiplicative cryptography.py
+++ multiplicative cryptography.py
@@ -1,8 +1,8 @@
 import math
 
-# The 'alphabet' is specific to this program, and does not contain many characters such as ^ and ;.
-alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?1234567890',.()[]/<>-_=+#*& "
-alphabet+='"'
+# The 'alphabet' is specific to this program, and does not contain many  characters such as ^ and ;.
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',.!?()[]/<>-_=+#*& "
+ALPHABET+='"'
 
 # Checks if the key is coprime with the alphabet
 def are_coprime(a, b):
@@ -28,36 +28,36 @@ def modular_inverse(a, m):
 
 
 # Takes parameter 'multiply_key', the number which will be used in the multiplicative algorithm, and then asks for a message.
-# The key MUST be coprime with the length of the alphabet or the program will not work correctly.
+# The key MUST be prime or the program will not work correctly.
 # The message is then run through the multiplicative algorithm to produce the encoded message. The encoded message will change
 # depending on the value of 'multiply_key'.
 def multiplicative_encode(multiply_key):
     message = input("What message would you like to encode?")
     for letter in message:
-        pos = alphabet.find(letter)
+        pos = ALPHABET.find(letter)
         if pos == -1:
             print("ERROR", end="")
         else:
-            convert = (pos * multiply_key) % len(alphabet)
-            print(alphabet[convert], end="")
+            convert = (pos * multiply_key) % len(ALPHABET)
+            print(ALPHABET[convert], end="")
 
 
-# Takes parameter 'multiply_key' and asks for an encoded message. If 'multiply_key' is not coprime with the length of the alphabet,
+# Takes parameter 'multiply_key' and asks for an encoded message. If 'multiply_key' is not coprime with the size of the alphabet,
 # the function will return an error. The key will be used in the algorithm to reverse-engineer the encoding process and return the decoded message.
 def multiplicative_decode(multiply_key):
-    if not are_coprime(multiply_key, len(alphabet)):
+    if not are_coprime(multiply_key, len(ALPHABET)):
         print("The key is not coprime with the size of the alphabet. Please use a valid key.")
         return
 
-    inverse_key = modular_inverse(multiply_key, len(alphabet))
+    inverse_key = modular_inverse(multiply_key, len(ALPHABET))
     encoded_message = input("What message would you like to decode? ")
     for letter in encoded_message:
-        pos = alphabet.find(letter)
+        pos = ALPHABET.find(letter)
         if pos == -1:
             print("ERROR", end="")
         else:
-            convert = (pos * inverse_key) % len(alphabet)
-            print(alphabet[convert], end="")
+            convert = (pos * inverse_key) % len(ALPHABET)
+            print(ALPHABET[convert], end="")
 
 # This is the part the user interacts with. It asks the user to choose to encode or decode a message, and if the response
 # matches neither, resets. It will run functions encode_multiplicative() or decode_multiplicative to encode or decode
@@ -73,14 +73,14 @@ if encode_or_decode == "encode":
         multiply_key = int(input("What is the key?"))
     except ValueError:
             multiply_key = int(input("That is not a number key. Please enter a valid key."))
-    while not are_coprime(multiply_key, len(alphabet)):
-        multiply_key = int(input("That key is not coprime with the length of the alphabet used here("+str(len(alphabet))+"). Please enter a valid key."))
+    while not are_coprime(multiply_key, len(ALPHABET)):
+        multiply_key = int(input("That key is not coprime with the length of the alphabet used here("+str(len(ALPHABET))+"). Please enter a valid key."))
     multiplicative_encode(multiply_key)
 elif encode_or_decode == "decode":
     try:
         multiply_key = int(input("What is the key?"))
     except ValueError:
         multiply_key = int(input("That is not a number key. Please enter a valid key."))
-    while not are_coprime(multiply_key, len(alphabet)):
-        multiply_key = int(input("That key is not coprime with the length of the alphabet used here("+str(len(alphabet))+"). Please enter a valid key."))
+    while not are_coprime(multiply_key, len(ALPHABET)):
+        multiply_key = int(input("That key is not coprime with the length of the alphabet used here("+str(len(ALPHABET))+"). Please enter a valid key."))
     multiplicative_decode(multiply_key)
