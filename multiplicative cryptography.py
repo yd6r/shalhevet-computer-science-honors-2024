7bd241c91176e5773635fae5d88977b5963e7577
Yehudah Deitcher <2027.y.deitcher@shalhevet.org>
2025-03-24 13:50:35 -0700

    Update multiplicative cryptography.py

--- multiplicative cryptography.py
+++ multiplicative cryptography.py
@@ -29,7 +29,7 @@ def modular_inverse(a, m):
 
 # Takes parameter 'multiply_key', the number which will be used in the multiplicative algorithm, and then asks for a message.
 # The key MUST be prime or the program will not work correctly. If any character in the message is not included in
-#ALPHABET, it will print ERROR and end the program. 
+#ALPHABET, it will print ERROR and end the program.
 # The message is then run through the multiplicative algorithm to produce the encoded message. The encoded message will change
 # depending on the value of 'multiply_key'.
 def multiplicative_encode(multiply_key):
@@ -44,8 +44,9 @@ def multiplicative_encode(multiply_key):
             print(ALPHABET[convert], end="")
 
 
-# Takes parameter 'multiply_key' and asks for an encoded message. If 'multiply_key' is not coprime with the size of the alphabet,
-# the function will return an error. The key will be used in the algorithm to reverse-engineer the encoding process and return the decoded message.
+# Takes parameter 'multiply_key' and asks for an encoded message. If 'multiply_key' is not coprime with the size of ALPHABET,
+# the function will return an error. In this version of the program, ALPHABET is prime, and therefore any key will work. 
+#The key will be used in the algorithm to reverse-engineer the encoding process and return the decoded message.
 def multiplicative_decode(multiply_key):
     if not are_coprime(multiply_key, len(ALPHABET)):
         print("The key is not coprime with the size of the alphabet. Please use a valid key.")
@@ -61,11 +62,12 @@ def multiplicative_decode(multiply_key):
             convert = (pos * inverse_key) % len(ALPHABET)
             print(ALPHABET[convert], end="")
 
+
 # This is the part the user interacts with. It asks the user to choose to encode or decode a message, and if the response
 # matches neither, resets. It will run functions encode_multiplicative() or decode_multiplicative to encode or decode
 # a given message.
 encode_or_decode = input("For encoding a message, type 'encode'. For decoding a message, type 'decode'.")
-if encode_or_decode != "encode" and encode_or_decode != "decode":
+if encode_or_decode != "encode" and encode_or_decode != "edecode":
     while encode_or_decode != "encode" and encode_or_decode != "decode":
         print("'" + encode_or_decode + "' is not a valid response.")
         encode_or_decode = input("For encoding a message, type 'encode'. For decoding a message, type 'decode'.")
